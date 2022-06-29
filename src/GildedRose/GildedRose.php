19497f5d4a1ae47e3eda13c009b9aa0a77fb997f
Rodrigo Lozano <lozano.tc7@hotmail.com>
2022-06-29 16:17:15 +0200

    Refactor step 1

--- src/GildedRose/GildedRose.php
+++ src/GildedRose/GildedRose.php
@@ -18,7 +18,7 @@ final class GildedRose
             function ($item) {
                 if ($item->name == 'Sulfuras, Hand of Ragnaros') {
                     $item->quality = 80;
-                } elseif($item->quality > 50){
+                } elseif ($item->quality > 50) {
                     $item->quality = 50;
                 }
             },
@@ -30,49 +30,51 @@ final class GildedRose
     public function updateQuality(): void
     {
         foreach ($this->items as $item) {
-            if ($item->name != 'Aged Brie' and $item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                if ($item->quality > 0) {
-                    if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                        $item->quality = $item->quality - 1;
-                    }
-                }
-            } else {
-                if ($item->quality < 50) {
-                    $item->quality = $item->quality + 1;
-                    if ($item->name == 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->sell_in < 11) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                        if ($item->sell_in < 6) {
-                            if ($item->quality < 50) {
-                                $item->quality = $item->quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
+            if($item->name == 'Aged Brie'){
+                $item->sell_in--;
 
-            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                $item->sell_in = $item->sell_in - 1;
-            }
+                if ($item->quality == 50) {
+                    $item->quality = 50;
+                } elseif ($item->sell_in >= 0) {
+                    $item->quality += 1;
+                } else {
+                    $item->quality += 2;
+                }
+            }elseif($item->name == 'Sulfuras, Hand of Ragnaros'){
+                $item->quality = 80;
+            }elseif($item->name == 'Backstage passes to a TAFKAL80ETC concert'){
+                $item->sell_in--;
 
-            if ($item->sell_in < 0) {
-                if ($item->name != 'Aged Brie') {
-                    if ($item->name != 'Backstage passes to a TAFKAL80ETC concert') {
-                        if ($item->quality > 0) {
-                            if ($item->name != 'Sulfuras, Hand of Ragnaros') {
-                                $item->quality = $item->quality - 1;
-                            }
-                        }
+                if ($item->sell_in < 0) {
+                    $item->quality = 0;
+                } elseif ($item->sell_in < 5) {
+                    if ($item->quality > 47) {
+                        $item->quality = 50;
                     } else {
-                        $item->quality = $item->quality - $item->quality;
+                        $item->quality += 3;
+                    }
+                } elseif ($item->sell_in < 10) {
+                    if ($item->quality > 48) {
+                        $item->quality = 50;
+                    } else {
+                        $item->quality += 2;
                     }
                 } else {
-                    if ($item->quality < 50) {
-                        $item->quality = $item->quality + 1;
+                    if ($item->quality > 49) {
+                        $item->quality = 50;
+                    } else {
+                        $item->quality++;
                     }
+                }
+            }else{
+                $item->sell_in--;
+
+                if($item->quality != 0){
+                    $item->quality--;
+                }
+                
+                if ($item->sell_in < 0 && $item->quality != 0) {
+                    $item->quality--;
                 }
             }
         }
