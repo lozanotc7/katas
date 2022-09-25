45fb470a25cf5ef15778c32d6460e97aa5d0a07e
Rodrigo Lozano <lozano.tc7@hotmail.com>
2022-09-25 11:06:43 +0200

    Repository example

--- src/Cart/Infrastructure/InMemoryCartRepository.php
+++ src/Cart/Infrastructure/InMemoryCartRepository.php
@@ -19,7 +19,9 @@ class InMemoryCartRepository implements CartRepository
                 new Uuid('aaaa-aaaa-aaaa-aaaa'),
         'First cart',
                 new ProductsOnCart(
-                    new Product(new Uuid('1111-1111'))
+                    new Product(new Uuid('690ae5a0-1a57-4549-b480-917c774aad2b'), "first product", 111.11),
+                    new Product(new Uuid('d6d494cf-638e-479d-a1aa-b47a1fedbd7c'), "second product", 222.22),
+                    new Product(new Uuid('ac16cb1b-654f-4c8e-8a65-a591c985fc18'), "third product", 333.33),
                 )),
             new Cart(),
             new Cart(),
