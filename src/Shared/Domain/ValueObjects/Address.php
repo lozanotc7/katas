1db1615623abdc010ebbfc6fc5c6ba094e8e578f
Rodrigo Lozano <lozano.tc7@hotmail.com>
2022-09-25 12:29:30 +0200

    modify valueObject

--- src/Shared/Domain/ValueObjects/Address.php
+++ src/Shared/Domain/ValueObjects/Address.php
@@ -16,4 +16,30 @@ final class Address
         public readonly ?string $door = null
     ) {
     }
+
+    public function to_array()
+    {
+        return [
+            "region" => $this->region,
+            "city" => $this->city,
+            "postalCode" => $this->postalCode,
+            "street" => $this->street,
+            "number" => $this->number,
+            "floor" => $this->floor,
+            "door" => $this->door,
+        ];
+    }
+
+    public static function from_array($array)
+    {
+        return new self(
+            $array["region"],
+            $array["city"],
+            $array["postalCode"],
+            $array["street"],
+            $array["number"],
+            $array["floor"],
+            $array["door"],
+        );
+    }
 }
