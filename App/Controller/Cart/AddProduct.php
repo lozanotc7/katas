8ed741f6d938a94fb1abc58a26f756cd128c5608
Rodrigo Lozano <lozano.tc7@hotmail.com>
2022-09-25 12:51:12 +0200

    Endpoint controller

--- App/Controller/Cart/AddProduct.php
+++ App/Controller/Cart/AddProduct.php
@@ -1,6 +1,6 @@
 <?php
 
-use Katas\Cart\Application\AddProductToCart;
+use Katas\Cart\Application\AddProductToCartFromUuids;
 use Katas\Cart\Domain\CartRepository;
 use Katas\Cart\Domain\ProductRepository;
 
@@ -13,20 +13,18 @@ use Katas\Cart\Domain\ProductRepository;
 
 class AddProduct extends AbstractController
 {
-    public function __construct(private CartRepository $cartRepository, private ProductRepository $productRepository)
+    public function __construct(private ProductRepository $productRepository, private CartRepository $cartRepository)
     {
     }
 
     public function add(Request $request)
     {
-        $useCase = new AddProductToCart($this->cartRepository);
-
         $data = $request->request();
 
-        $product = $this->productRepository->find($data->get('product.id'));
-        $cart    = $this->cartRepository->find($data->get('product.id'));
-
-        $useCase($product, $cart);
+        (new AddProductToCartFromUuids($this->productRepository, $this->cartRepository))(
+            $data->get('product.id'),
+            $data->get('cart.id')
+        );
 
         return new JsonResponse([], 200);
     }
